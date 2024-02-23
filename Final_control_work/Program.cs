9cfe466fcffa605d6cfe9466209e7d186d5c8e5f
Kirill <kz@selfiemosaic.ru>
2024-02-23 13:10:27 +0300

    Working v_1

--- Final_control_work/Program.cs
+++ Final_control_work/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// sdsfdsdf
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string []  Find_string_in_array (string [] array)
+{
+    int length = array.Length;
+    string [] result = new string[length];
+    
+    
+    
+    for(int i = 0; i < length; i++)
+    {
+        if(array[i].Length <= 3 )
+        {
+            result[i] = array[i];
+        }
+    }
+    return result;
+
+} 
+
+string [] input = {"Hello", "World", "from", "SPB", "!"};
+
+Console.WriteLine($"Массив из строк длиной мене либо равной 3 символам: [{string.Join(", ", Find_string_in_array(input))}]: ");
